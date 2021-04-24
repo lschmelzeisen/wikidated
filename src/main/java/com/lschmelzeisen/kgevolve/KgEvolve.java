8f3574a6bb9302c7bff1171dcfc256ea569a5cab
Lukas Schmelzeisen <me@lschmelzeisen.com>
2021-04-24 08:57:21 +0200

    Implement rudimentary output to RDF.

--- src/main/java/com/lschmelzeisen/kgevolve/KgEvolve.java
+++ src/main/java/com/lschmelzeisen/kgevolve/KgEvolve.java
@@ -20,9 +20,13 @@ import examples.ExampleHelpers;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import org.wikidata.wdtk.datamodel.helpers.Datamodel;
 import org.wikidata.wdtk.datamodel.interfaces.EntityDocumentProcessorBroker;
+import org.wikidata.wdtk.datamodel.interfaces.Sites;
 import org.wikidata.wdtk.dumpfiles.EntityTimerProcessor;
-import org.wikidata.wdtk.dumpfiles.WikibaseRevisionProcessor;
+import org.wikidata.wdtk.dumpfiles.MwLocalDumpFile;
+import org.wikidata.wdtk.dumpfiles.MwSitesDumpFileProcessor;
+import org.wikidata.wdtk.rdf.PropertyRegister;
 
 public class KgEvolve {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -34,15 +38,31 @@ public class KgEvolve {
                         // "wikidatawiki-20210401-pages-meta-history1.xml-p1p192.7z");
                         "wikidatawiki-20210401-pages-meta-history25.xml-p67174382p67502430.7z");
 
+        MwLocalDumpFile sitesTableDump =
+                new MwLocalDumpFile(
+                        "dumpfiles/wikidatawiki/sites-20210401/wikidatawiki-20210401-sites.sql.gz");
+        MwSitesDumpFileProcessor sitesDumpFileProcessor = new MwSitesDumpFileProcessor();
+        sitesDumpFileProcessor.processDumpFileContents(
+                sitesTableDump.getDumpFileStream(), sitesTableDump);
+        Sites sites = sitesDumpFileProcessor.getSites();
+
         var entityTimerProcessor = new EntityTimerProcessor(0);
+
+        var rdfSerializer =
+                new MyRdfSerializer(
+                        Paths.get("triples-ALL_ENTITIES_ALL_EXACT_DATA.ttl"),
+                        sites,
+                        PropertyRegister.getWikidataPropertyRegister(),
+                        Datamodel.SITE_WIKIDATA);
 
         var entityDocumentProcessor = new EntityDocumentProcessorBroker();
         entityDocumentProcessor.registerEntityDocumentProcessor(entityTimerProcessor);
+        //        entityDocumentProcessor.registerEntityDocumentProcessor(rdfSerializer);
 
-        var dumpProcessor =
-                new FastRevisionDumpFileProcessor(
-                        new WikibaseRevisionProcessor(
-                                entityDocumentProcessor, "http://www.wikidata.org/"));
+        var dumpProcessor = new FastRevisionDumpFileProcessor(rdfSerializer);
+        //                        entityTimerProcessor);
+        //                                new WikibaseRevisionProcessor(
+        //                                entityDocumentProcessor, "http://www.wikidata.org/"));
 
         entityTimerProcessor.open();
         dumpProcessor.processDumpFile(dumpFile);
