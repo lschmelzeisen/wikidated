b4a1198bd5dba207bd7b219c5c146dc6e57afcd8
Lukas Schmelzeisen <me@lschmelzeisen.com>
2021-09-15 16:36:48 +0200

    Refactor JvmManager into new wikidated package.

---
+++ src/wikidated/_jvm_manager.py
@@ -0,0 +1,131 @@
+#
+# Copyright 2021 Lukas Schmelzeisen
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from __future__ import annotations
+
+from logging import FileHandler, Formatter, Handler, Logger, LogRecord, getLogger
+from pathlib import Path
+from types import TracebackType
+from typing import TYPE_CHECKING, Any, Callable, MutableMapping, Optional, Type, TypeVar
+
+if TYPE_CHECKING:
+    _F = TypeVar("_F", bound=Callable[..., Any])
+
+    def JOverride(_f: _F) -> _F:  # noqa: N802
+        ...
+
+
+from jpype import JOverride  # type: ignore # noqa: F811
+from jpype import JClass, JImplements, JObject, shutdownJVM, startJVM
+
+_LOGGER = getLogger(__name__)
+
+
+class JvmManager:
+    def __init__(self, *, jars_dir: Path) -> None:
+        self.jars_dir = jars_dir
+
+        _LOGGER.debug("Starting JVM...")
+        startJVM(classpath=[str(self.jars_dir / "*")])
+
+        self._java_logging_bridge = _JavaLoggingBridge()
+
+    def close(self) -> None:
+        assert self  # Stop PyCharm from suggesting to make this method static.
+
+        _LOGGER.debug("Shutting down JVM...")
+        shutdownJVM()
+
+    def __enter__(self) -> JvmManager:
+        return self
+
+    def __exit__(
+        self,
+        _exc_type: Optional[Type[BaseException]],
+        _exc_val: Optional[BaseException],
+        _exc_tb: Optional[TracebackType],
+    ) -> None:
+        self.close()
+
+    def set_logging_file_handler(
+        self, file: Optional[Path], formatter: Optional[Formatter] = None
+    ) -> None:
+        if not file:
+            self._java_logging_bridge.set_file_handler(None)
+            return
+
+        file_handler = FileHandler(file)
+        file_handler.setFormatter(
+            formatter
+            or Formatter("{asctime} {levelname:.1} [{name}] {message}", style="{")
+        )
+        self._java_logging_bridge.set_file_handler(file_handler)
+
+
+@JImplements("java.util.logging.Filter", deferred=True)
+class _JavaLoggingBridge:
+    def __init__(self) -> None:
+        _LOGGER.debug("Starting Java logging bridge...")
+
+        self._loggers: MutableMapping[str, Logger] = {}
+        self._file_handler: Optional[Handler] = None
+        self._formatter = JClass("java.util.logging.SimpleFormatter")()
+
+        log_manager = JClass("java.util.logging.LogManager").getLogManager()
+        log_manager.reset()
+
+        dummy_handler = JClass("java.util.logging.ConsoleHandler")()
+        dummy_handler.setFilter(self)
+
+        root_logger = log_manager.getLogger("")
+        root_logger.setLevel(JClass("java.util.logging.Level").ALL)
+        root_logger.addHandler(dummy_handler)
+
+    def set_file_handler(self, file_handler: Optional[Handler]) -> None:
+        self._file_handler = file_handler
+
+    @JOverride
+    def isLoggable(self, record: JObject) -> bool:  # noqa: N802
+        name = f"jpype.{record.getLoggerName()}"
+        # Transform Java log level to Python log level
+        level = max(record.getLevel().intValue() // 10 - 60, 10)
+        # Format message with potential parameters. (Don't know how to postpone this
+        # until after we are sure the message will be displayed.)
+        message = str(self._formatter.formatMessage(record))
+
+        logger = self._loggers.get(name)
+        if logger is None:
+            logger = getLogger(name)
+            self._loggers[name] = logger
+
+        logger.log(level, message)
+        if self._file_handler:
+            self._file_handler.handle(
+                LogRecord(
+                    name=name,
+                    level=level,
+                    pathname="Unknown.java",
+                    lineno=-1,
+                    msg=message,
+                    args=(),
+                    exc_info=None,
+                    func=None,
+                    sinfo=None,
+                )
+            )
+
+        # Causes the Java-configured logging handler to discard all messages.
+        return False
--- vulture-whitelist.py
+++ vulture-whitelist.py
@@ -2,3 +2,4 @@ _.error_on_external_run  # unused attribute (/home/lschmelzeisen/Repositories/wi
 _.reuse_existing_virtualenvs  # unused attribute (/home/lschmelzeisen/Repositories/wikidated/noxfile.py:23)
 _.stop_on_first_error  # unused attribute (/home/lschmelzeisen/Repositories/wikidated/noxfile.py:24)
 test  # unused function (/home/lschmelzeisen/Repositories/wikidated/noxfile.py:44)
+_.isLoggable  # unused method (/home/lschmelzeisen/Repositories/wikidated/src/wikidated/_jvm_manager.py:100)
