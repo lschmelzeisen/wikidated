186ca02fc02e8b9911ddb6380065aef934587f83
Lukas Schmelzeisen <me@lschmelzeisen.com>
2021-10-27 16:22:14 +0200

    Simplify rangemap implementation and comment complexities.

--- src/wikidated/_utils/range_map.py
+++ src/wikidated/_utils/range_map.py
@@ -48,7 +48,8 @@ class RangeMap(
             return False
 
     def _index(self, key: int) -> int:
-        # Binary search for first element with range start above given key.
+        # Binary search for last item with start of range lesser or equal to given key.
+        # Returns -1 if all items have a larger start of range.
         left = 0  # Inclusive.
         right = len(self._data)  # Exclusive.
         while left != right:
@@ -58,12 +59,14 @@ class RangeMap(
             else:
                 left = i + 1
         # assert key < self._data[left][0].start # noqa: E800
-        return left
+        return left - 1
 
     def __setitem__(self, key: object, value: _T_Value) -> None:
+        # O(1) for keys inserted in increasing order, O(n) otherwise.
+
         if not isinstance(key, range) or key.step != 1:
             raise TypeError("key must be a range with step = 1.")
-        if not len(key):
+        if not key:
             raise TypeError("key must not be an empty range.")
 
         if not self._data or key.start >= self._data[-1][0].stop:
@@ -71,45 +74,48 @@ class RangeMap(
             return
 
         i = self._index(key.start)
-        if key == self._data[i - 1][0]:
-            # Overwrite existing element.
-            self._data[i - 1] = (key, value)
+        if key == self._data[i][0]:
+            # Overwrite existing item.
+            self._data[i] = (key, value)
             return
 
-        if key.stop > self._data[i][0].start or (
-            i != 0 and key.start < self._data[i - 1][0].stop
+        if key.stop > self._data[i + 1][0].start or (
+            i != -1 and key.start < self._data[i][0].stop
         ):
             raise TypeError("Overlapping ranges.")
 
-        self._data[i + 1 :] = self._data[i:]  # Expensive but unavoidable with our impl.
-        self._data[i] = (key, value)
+        self._data.insert(i + 1, (key, value))
 
     def __getitem__(self, key: object) -> _T_Value:
+        # O(log n).
+
         if isinstance(key, int):
             i = self._index(key)
-            if i != 0 and key in self._data[i - 1][0]:
-                return self._data[i - 1][1]
+            if i != -1 and key in self._data[i][0]:
+                return self._data[i][1]
         elif isinstance(key, range):
             i = self._index(key.start)
-            if i != 0 and key == self._data[i - 1][0]:
-                return self._data[i - 1][1]
+            if i != -1 and key == self._data[i][0]:
+                return self._data[i][1]
         else:
             raise TypeError("key must either be an int or a range.")
         raise KeyError(key)
 
     def __delitem__(self, key: object) -> None:
+        # O(n).
+
         if not isinstance(key, range):
             raise TypeError("key must be a range.")
 
         i = self._index(key.start)
-        if i == 0 or self._data[i - 1][0] != key:
-            raise KeyError(key)
+        if i != -1 and key == self._data[i][0]:
+            del self._data[i - 1]
 
-        del self._data[i - 1]
+        raise KeyError(key)
 
     def __iter__(self) -> Iterator[range]:
-        for elem_key, _elem_value in self._data:
-            yield elem_key
+        for item_key, _item_value in self._data:
+            yield item_key
 
     def clear(self) -> None:
         self._data.clear()
