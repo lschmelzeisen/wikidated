b14826c326496008169f148f2ddd3327b93194cb
Lukas Schmelzeisen <me@lschmelzeisen.com>
2021-12-13 12:22:53 +0100

    Make exceptions show up in log.

--- scripts/build_wikidated_v1_0.py
+++ scripts/build_wikidated_v1_0.py
@@ -15,20 +15,29 @@
 #
 
 from datetime import date
+from logging import getLogger
 from pathlib import Path
 
 from wikidated.wikidated_manager import WikidatedManager
 
+_LOGGER = getLogger(__name__)
+
 if __name__ == "__main__":
-    data_dir = Path("data")
-    data_dir.mkdir(exist_ok=True, parents=True)
+    try:
+        data_dir = Path("data")
+        data_dir.mkdir(exist_ok=True, parents=True)
 
-    wikidated_manager = WikidatedManager(data_dir)
-    wikidated_manager.configure_logging(log_wdtk=True)
-    wikidated_manager.download_java_dependencies()
+        wikidated_manager = WikidatedManager(data_dir)
+        wikidated_manager.configure_logging(log_wdtk=True)
+        wikidated_manager.download_java_dependencies()
 
-    wikidata_dump = wikidated_manager.wikidata_dump(date(year=2021, month=6, day=1))
-    wikidata_dump.download()
+        wikidata_dump = wikidated_manager.wikidata_dump(date(year=2021, month=6, day=1))
+        wikidata_dump.download()
 
-    wikidated_dataset = wikidated_manager.custom(wikidata_dump)
-    wikidated_dataset.build(max_workers=4)
+        wikidated_dataset = wikidated_manager.custom(wikidata_dump)
+        wikidated_dataset.build(max_workers=4)
+
+    except Exception:
+        # Make exceptions show up in log.
+        _LOGGER.exception("Exception occurred.")
+        raise
